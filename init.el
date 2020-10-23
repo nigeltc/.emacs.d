aa0d660e90173cba1fc8512009fc90834449d512
Nigel Thornton Clark <nigeltc@gmail.com>
2020-10-23 13:11:29 -0400

    Fix elpa problem, add zenburn theme

--- init.el
+++ init.el
@@ -8,17 +8,3 @@
 (org-babel-load-file
  (expand-file-name "~/.emacs.d/ntc.org"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (bufler htmlize counsel yasnippet web-mode w3m use-package undo-tree try swiper neotree markdown-mode lorem-ipsum jedi indent-guide iedit google-c-style flycheck expand-region doom-themes doom-modeline beacon ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
